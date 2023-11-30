42fefacbcf1d8007a047fc21e71d9d9743ac215d
Kunduz Mutalip <144219469+creatnameKunduz@users.noreply.github.com>
2023-11-30 22:43:47 +0600

    Add files via upload

---
+++ thirdDay/app.js
@@ -0,0 +1,64 @@
+    let  x = 78
+    let y = 90
+    let z =1/2 // так определяеться корень
+        console.log(y+x);
+        console.log(y-x);// вычетание
+        console.log(x**y);
+        console.log(x**z);//возведения в степень
+        console.log(x%z); //вывести остаток
+
+    // ИНКРЕМЕНТ увелечивает на 1
+    let count = 56
+    count++;
+    console.log(count);
+    //ДИКРЕМЕНТ  уменшает на 1
+
+    let score = 0
+    score--;
+    console.log(score);
+     var years =  2023
+    years++;
+     console.log(years);
+     years = 2  +years;
+       var age = 17;
+       age--;
+       console.log(age)
+
+
+    var n1 = 5
+    var n2 = 50
+    n3 = n1 -- + n2;
+       console.log(n3);
+       n1 --;
+       n4 = n1 + n2
+       console.log(n4)
+
+    // инсремент и дикремент работает не только с числами а также с логическими, строгими итд
+    let  namber = 2.3
+    namber++;
+       console.log(namber)
+
+
+
+
+    let  m1 = 12
+    let  m2 = 10
+    m3= m1*m2
+    m4 = m3+7
+    console.log(m4)
+    alert(m4)
+
+    let num
+    num=prompt('Задумайте число');
+       num =parseFloat(num);
+       let result = num * 2+7;
+       alert('Вы задумали число'+ result)
+
+
+    let name
+    name=prompt('Введите имя и фамилию студента')
+    name2=prompt('Введите имя и фамилию студента')
+    name3=prompt('Введите имя и фамилию студента')
+
+
+

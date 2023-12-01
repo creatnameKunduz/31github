146f40092754f163bf7ab4d9ee326b44cc16ddfa
Kunduz Mutalip <144219469+creatnameKunduz@users.noreply.github.com>
2023-12-01 07:16:32 +0600

    Add files via upload

---
+++ fourthDay/app.js
@@ -0,0 +1,28 @@
+
+var month = prompt('Enter month');
+
+switch (month.toLowerCase()) {
+    case 'may':
+        console.log('spring');
+        break;
+
+    case 'june':
+        console.log('summer');
+        break;
+
+    case 'february':
+        console.log('winter');
+        break;
+
+    case 'september':
+        console.log('autumn');
+        break;
+
+    default:
+        console.log('Enter correctly something');
+        break; // Добавленный оператор break для default
+}
+
+
+//array -массивы
+

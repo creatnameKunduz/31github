fa2f1f6f6194fb7c0ee447240faebbd502834794
Kunduz Mutalip <144219469+creatnameKunduz@users.noreply.github.com>
2023-12-02 23:03:51 +0600

    Add files via upload

---
+++ fifthDay/app.js
@@ -0,0 +1,46 @@
+    let  name  = 'Kunduz'
+    let fullName = ' Kunduz Mutalip'
+    let age  = 18
+    let year = 2005
+    console.log('MY FULLNAME -  '  +  fullName)
+    console.log( typeof age )
+    console.log(name.toLowerCase())
+    console.log(name.toUpperCase())
+    console.log(name.repeat(29))
+
+
+     console.log( year / age )
+     console.log( year *  age )
+     console.log( year ** age )
+     console.log( year % age )
+    console.log('Average year 32-03-> ', (17 + 12 + 13 + 14 + 15 + 16 + 17 + 18 + 19) / 9  ) // ctrl+alt+l отформатирование
+
+
+    // boolean if,  else, else... if,
+    var numFirst = 2005
+    var numSecond = 2006
+    console.log(numFirst> numSecond)
+    console.log(numFirst === numSecond) // строгое равенство
+    console.log(numFirst == numSecond) //не строгое равенство
+    console.log(numFirst <= numSecond)
+    console.log(numFirst < numSecond)
+    console.log(numFirst !== numSecond) //   неравество
+
+    var value= prompt('value')
+    var valueSecond = prompt('valueSecond')
+    if( value > valueSecond ){
+        alert(' value power')
+    }else if (valueSecond> value){
+        alert('valueSecond power')
+    } else{
+        alert(" you don't have another choose")
+    }
+
+
+
+
+
+
+
+
+

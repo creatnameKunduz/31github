21e053d724cb7999e3f67bb2853acba54c423aad
Kunduz Mutalip <144219469+creatnameKunduz@users.noreply.github.com>
2023-12-03 07:27:57 +0600

    Add files via upload

---
+++ seconDay/app.js
@@ -0,0 +1,22 @@
+let company = {
+    post: 'DEVELOPER',
+    name: 'IVAN'
+}
+alert(company.post);
+
+
+
+let userName = 'Иван';
+
+function showMessage () {
+    let message = 'Привет,'+userName;
+    alert(message);
+}
+showMessage();
+
+
+let color  = ['yellow', 'red', 'green'];
+color[0]='blue';
+color[1]='white';
+color[2]='black';
+ alert(color);

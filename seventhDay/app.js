f891c3d890b598a2794919d1e5269bcb715232de
Kunduz Mutalip <144219469+creatnameKunduz@users.noreply.github.com>
2023-12-04 18:54:57 +0600

    Add files via upload

---
+++ seventhDay/app.js
@@ -0,0 +1,32 @@
+let num =5
+let x=5, y=6, z=7;
+let score =100
+score=score+10;
+console.log(score);
+x+=5;
+x-=5;
+x/=5;
+x*=5;
+x%=5;
+
+ let  k=8;
+ k++;
+ k/=3;
+console.log(k) //ответ 3 сначала доб 1 8+1=9 9/3= 3
+
+let price =50;
+let rate = 1.2;
+price*=rate; // price= price*rate;
+
+// \ - этот знак escape-символ. используется для переход новую строку табуляция
+
+//console.log('I'm happy'); // mistake
+console.log('I\'m happy');
+console.log("She said \" Yes!\"");
+
+// для создания нов строки в тексте n\
+console.log("One\nTwo \nTree ")
+
+// табуляция типа выводит неслитно
+console.log("\t hey \t there");
+
